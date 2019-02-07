9881164b4aa58e579a6a1fcda3175f85957c2e19
drgranda4 <43816643+drgranda4@users.noreply.github.com>
2019-02-06 21:10:45 -0500

    Update main.cpp

--- Proyecto 3/main.cpp
+++ Proyecto 3/main.cpp
@@ -1,9 +1,41 @@
 #include <iostream>
+#include <math.h>
+// obtener el cuadrado y el cubo de un numero ingresado por el usuario
 
 using namespace std;
+int obtenerCuadrado (int numero){
+     int  resultado = pow(numero , 2 );
+     return resultado;
+}
+int  obtenerCubo (int numero ){
+      int resultado = pow(numero , 3);
+      return resultado;
+}
 
 int main()
 {
-    cout << "Hello world!" << endl;
+    double numero = 0;
+    int opcion = 0;
+    int resultado = 0;
+
+      cout << "Ingrese una opcion para operar" << endl;
+    cin >> opcion;
+
+    cout << "Ingrese un numero para operar" << endl;
+    cin >> numero;
+
+
+    if (opcion == 1){
+    resultado = obtenerCuadrado(numero);
+        cout << "El cuadrado del numero " << numero << "es :" << resultado << endl;
+    }else{
+        if (opcion == 2){
+            resultado = obtenerCubo(numero);
+            cout << "El cubo del numero " << numero << "es :" << resultado << endl;
+        }else{
+            cout << "Opción incorrecta "  << endl;
+        }
+    }
+
     return 0;
 }
